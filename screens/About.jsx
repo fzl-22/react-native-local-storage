5ece2e7903218ded083ed68185b1c1674cfbdcd8
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2023-11-28 09:31:10 +0700

    chore: done initial project

---
+++ screens/About.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button, Linking } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const About = () => {
+  const handleClearData = async () => {
+    try {
+      await AsyncStorage.clear();
+      console.log('Data Cleared');
+    } catch (e) {
+      console.log('Error clear data: in about.js');
+      console.error(e);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>About</Text>
+      <Text style={styles.title}>About This Application</Text>
+      <Text style={styles.content}>
+        Aplikasi ini dirancang sebagai studi kasus untuk pembelajaran mata
+        kuliah Pemrograman Mobile Program Studi Informatika Institut Teknologi
+        Telkom Surabaya
+      </Text>
+      <Text
+        style={{ marginBottom: 5 }}
+        onPress={() =>
+          Linking.openURL(
+            'https://www.freepik.com/icon/task-list_9329651#fromView=search&term=todo+list&page=1&position=1&track=ais'
+          ).catch((err) => console.error('Error', err))
+        }
+      >
+        Icon by Azland Studio (Freepik)
+      </Text>
+      <Text
+        style={{ marginBottom: 15 }}
+        onPress={() =>
+          Linking.openURL('https://github.com/fzl-22').catch((err) =>
+            console.error('Error', err)
+          )
+        }
+      >
+        Developed by Ahmad Mu'min Faisal (1203210101)
+      </Text>
+
+      <Button
+        style={{ marginTop: 15 }}
+        title="Clear Data"
+        onPress={() => handleClearData()}
+      ></Button>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingLeft: 20,
+    paddingRight: 20,
+    marginTop: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  heading: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 7,
+    color: 'blue',
+  },
+  content: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+});
+
+export default About;
---
+++ screens/index.js
@@ -0,0 +1,5 @@
+import AboutScreen from "./About";
+import TaskScreen from "./TaskAll";
+import TaskCompletedScreen from "./TaskCompleted";
+
+export { AboutScreen, TaskCompletedScreen, TaskScreen };
